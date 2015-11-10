d4726a3680044ebd9c89e64f7b717842a9074a51
Raven <craven@crowz.kr>
2015-11-11 01:39:57 +0900

    1.SW1 button 입력받아 beacon 한번씩 send 하는 기능 적용 2.DeepSleep 기능 적용 3.Eclipse 로 분석하기 위한 프로젝트 파일 추가

--- beacon.c
+++ beacon.c
@@ -18,6 +18,17 @@
 #include <config_store.h>
 #include <pio.h>
 #include <random.h>
+
+/*============================================================================*
+ *  Private Definitions
+ *============================================================================*/
+
+#define PIO_LED0        10          /* PIO connected to the LED0 on CSR10xx */
+#define PIO_BUTTON      11          /* PIO connected to the button on CSR10xx */
+
+#define PIO_DIR_OUTPUT  TRUE        /* PIO direction configured as output */
+#define PIO_DIR_INPUT   FALSE       /* PIO direction configured as input */
+
 /*============================================================================*
  *  Local Header File
  *============================================================================*/
@@ -29,12 +40,86 @@
  *============================================================================*/
 
 static void startAdvertising(void);
+static void stopAdvertising(void);
 
 static void appSetRandomAddress(void);
+/*============================================================================*
+ *  Private Data Types
+ *============================================================================*/
+
+
+/*============================================================================*
+ *  Private Data
+ *============================================================================*/
+
+
+/* Current state of LED sequence */
+static char g_cur_led_seq = 0;
+
+/*============================================================================*
+ *  Private Function Prototypes
+ *============================================================================*/
+
+/* Advance LED flashing sequence to next state */
+static void goToNextLedSeq(void);
+
+/* Restart LED sequence from the beginning */
+static void restartLedSeq(void);
 
 /*============================================================================*
  *  Private Function Implementations
  *============================================================================*/
+/*----------------------------------------------------------------------------*
+ *  NAME
+ *      goToNextLedSeq
+ *
+ *  DESCRIPTION
+ *      Advance LED flashing sequence to next state.
+ *
+ * PARAMETERS
+ *      None
+ *
+ * RETURNS
+ *      Nothing
+ *----------------------------------------------------------------------------*/
+static void goToNextLedSeq(void)
+{
+	g_cur_led_seq = !g_cur_led_seq;
+
+	if(g_cur_led_seq == 0)
+	{
+		stopAdvertising();
+		/* Set LED0 according to bit 0 of desired pattern */
+		PioSet(PIO_LED0, 0);
+	}
+	else
+	{
+		startAdvertising();
+		PioSet(PIO_LED0, 1);
+	}
+}
+
+/*----------------------------------------------------------------------------*
+ *  NAME
+ *      restartLedSeq
+ *
+ *  DESCRIPTION
+ *      Restart LED sequence from the beginning.
+ *
+ * PARAMETERS
+ *      None
+ *
+ * RETURNS
+ *      Nothing
+ *----------------------------------------------------------------------------*/
+static void restartLedSeq(void)
+{
+    /* Turn off both LEDs by setting output to Low */
+    PioSets((1UL << PIO_LED0), 0UL);
+
+    /* LED sequence is reset to idle */
+    g_cur_led_seq = 0;
+}
 
 /*----------------------------------------------------------------------------*
  *  NAME
@@ -121,7 +206,8 @@ void startAdvertising(void)
     
     /* read User key 2 for the advertising interval */
     advInterval = CSReadUserKey(2);
-    
+
+
     /* range check */
     if((advInterval < MIN_ADVERTISING_INTERVAL) ||
        (advInterval > MAX_ADVERTISING_INTERVAL))
@@ -129,7 +215,7 @@ void startAdvertising(void)
         /* revert to default advertising interval */
         advInterval = DEFAULT_ADVERTISING_INTERVAL;
     }
-    
+
     /* read address type from User key 3 */
     if(CSReadUserKey(3))
     {
@@ -172,7 +258,10 @@ void startAdvertising(void)
     /* Start broadcasting */
     LsStartStopAdvertise(TRUE, whitelist_disabled, addressType);
 }
-
+void stopAdvertising(void)
+{
+    LsStartStopAdvertise(FALSE, whitelist_disabled, ls_addr_type_random);
+}
 
 /*============================================================================*
  *  Public Function Implementations
@@ -219,14 +308,45 @@ void AppInit(sleep_state last_sleep_state)
     PioSetDirs(0xFFFFFFFFUL, FALSE);
     PioSetPullModes(0xFFFFFFFFUL, pio_mode_strong_pull_down);
     
+    /* Set LED0 and LED1 to be controlled directly via PioSet */
+    PioSetModes((1UL << PIO_LED0), pio_mode_user);
+
+    /* Configure LED0 and LED1 to be outputs */
+    PioSetDir(PIO_LED0, PIO_DIR_OUTPUT);
+
+    /* Set the LED0 and LED1 to have strong internal pull ups */
+    PioSetPullModes((1UL << PIO_LED0),
+                    pio_mode_strong_pull_up);
+
+    /* Configure button to be controlled directly */
+    PioSetMode(PIO_BUTTON, pio_mode_user);
+
+    /* Configure button to be input */
+    PioSetDir(PIO_BUTTON, PIO_DIR_INPUT);
+
+    /* Set weak pull up on button PIO, in order not to draw too much current
+     * while button is pressed
+     */
+    PioSetPullModes((1UL << PIO_BUTTON), pio_mode_weak_pull_up);
+
+    /* Set the button to generate sys_event_pio_changed when pressed as well
+     * as released
+     */
+    PioSetEventMask((1UL << PIO_BUTTON), pio_event_mode_falling);
+
+
     /* disable wake up on UART RX */
     SleepWakeOnUartRX(FALSE);
     
     /* pull down the I2C lines */
     PioSetI2CPullMode(pio_i2c_pull_mode_strong_pull_down);
-       
+
+    /* Reset LED sequence */
+    restartLedSeq();
+
+	SleepModeChange(sleep_mode_deep);
     /* Start advertising */
-    startAdvertising();
+    //startAdvertising();
 }
 
 
@@ -245,7 +365,22 @@ void AppInit(sleep_state last_sleep_state)
 
 void AppProcessSystemEvent(sys_event_id id, void *data)
 {
-    /* empty */
+    /* If the reported system event is generated by a PIO */
+    if (id == sys_event_pio_changed)
+    {
+#if 0
+        /* The PIO data is defined by struct pio_changed_data */
+        const pio_changed_data *pPioData = (const pio_changed_data *)data;
+
+        /* If the PIO event comes from the button */
+        if (pPioData->pio_cause & (1UL << PIO_BUTTON))
+        {
+			goToNextLedSeq();
+        }
+#else // just changed PIO
+		goToNextLedSeq();
+#endif
+    }
 }
 
 
